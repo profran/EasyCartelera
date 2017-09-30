84912b60278a1edfa4d5091f101f886406fba8be
Francesco Silvetti <francescosilvetti8@gmail.com>
2017-09-30 18:23:12 -0300

    Navigation bar updated and UI improvements

--- js/map.js
+++ js/map.js
@@ -1,8 +1,18 @@
 function myMap() {
+
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        center: new google.maps.LatLng(-31.421632,-64.186935),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	map
+	
+	var marker = new google.maps.Marker({
+    	position: new google.maps.LatLng(-31.421632,-64.186935),
+    	map: map,
+    	dragable: true,
+    	title: 'Hello World!'
+  	});
 }
